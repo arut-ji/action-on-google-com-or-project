0a2b6076a5abd6e40ee31ab7eadf6eba501661e9
arut klaewkasetkorn <60090002@kmitl.ac.th>
2018-12-20 00:41:09 +0700

    erase mqtt

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,6 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 admin.initializeApp(functions.config().firebase);
 
-const mqtt = require("mqtt");
-const url = require("url");
 const db = admin.database();
 
 const { dialogflow } = require("actions-on-google");
